e83837f27b4306b1e2b89d38c302e381cde9f9f3
Ibad Desmukh <ibaddesmukh@gmail.com>
2025-04-04 10:18:04 +0100

    Add initial header

---
+++ book-1/implementations/karatsuba_mult.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2025 Ibad Desmukh
+#
+# SPDX-License-Identifier: MIT
+#
+"""Implement the karatsuba multiplication algorithm.
+
+Uses three recursive calls, instead of four recursive calls as per standard
+recursive integer multiplication.
+
+Reference:
+Roughgarden, T. (2017). Algorithms illuminated: Part 1: The Basics.
+Soundlikeyourself Publishing, LLC.
+"""
