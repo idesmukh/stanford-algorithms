d2d2b1dbe78e17738c08112ada053702cb5a9535
Ibad Desmukh <ibaddesmukh@gmail.com>
2025-04-04 11:06:11 +0100

    Implement base case

--- book-1/implementations/merge_sort.py
+++ book-1/implementations/merge_sort.py
@@ -8,3 +8,18 @@ Reference:
 Roughgarden, T. (2017). Algorithms illuminated: Part 1: The Basics.
 Soundlikeyourself Publishing, LLC.
 """
+
+import math
+
+
+def merge_sort(a: list) -> list:
+
+	# Define base case.
+	# If array length is 0 or 1, it is already sorted.
+	if len(a) <= 1:
+		return a
+
+if __name__ == "__main__":
+    # Verify the algorithm with a simple test case.
+    assert merge_sort([1]) == [1]
+    print("All tests passed!")
