9083595e05bc840a1a6fc352e4f23accb0ee572e
Ibad Desmukh <ibaddesmukh@gmail.com>
2025-04-04 10:47:57 +0100

    Add initial header

---
+++ book-1/implementations/merge_sort.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2025 Ibad Desmukh
+#
+# SPDX-License-Identifier: MIT
+#
+"""Implement the merge sort algorithm.
+
+Reference:
+Roughgarden, T. (2017). Algorithms illuminated: Part 1: The Basics.
+Soundlikeyourself Publishing, LLC.
+"""
